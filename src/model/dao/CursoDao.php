b9fda3c39e4e6eb2c99f0df2716417824e9d5fcd
Gustavo <82328081+GustavoLuisdO@users.noreply.github.com>
2021-10-30 20:07:53 -0300

    Update CursoDao.php

--- src/model/dao/CursoDao.php
+++ src/model/dao/CursoDao.php
@@ -1,9 +1,9 @@
 <?php    
 
     namespace sistema\src\Model\dao;
-    use \Model\bean\curso;
 
-    
+    require __DIR__.'/../../../vendor/autoload.php';
+    use \Model\bean\curso;
 
     require __DIR__ .'/../conexao.php';
     require __DIR__ .'/../bean/curso.php';
