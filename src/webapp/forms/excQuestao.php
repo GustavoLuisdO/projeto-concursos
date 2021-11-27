16986e1b055a3cb7829d8f73be0bf7e312e46067
Gustavo <gustavoluis202@gmail.com>
2021-11-26 22:09:12 -0300

    v3.6.7.2
    todo projeto está padronizado e com as funcionalidades corretas

--- src/webapp/forms/excQuestao.php
+++ src/webapp/forms/excQuestao.php
@@ -134,12 +134,20 @@
           <div class="col-8"></div>
         </div>
 
+        <hr>
+
         <!-- disciplinas -->
+        <div class="row text-center">
+          <div class="col-4"></div>
+          <div class="col-4">
+            <label>Disciplina(s)</label>
+          </div>
+          <div class="col-4"></div>
+        </div>
+
         <div class="row">
-           
           <!-- disciplina 1 -->
           <div class="form-group col-3">
-              <label for="id_disciplina_1">Disciplina</label>
               <select class="form-control" name="id_disciplina_1" id="id_disciplina_1" readonly>
                 <?php 
                   //mostrar a disciplina atual
@@ -172,7 +180,6 @@
             }
           ?>
           <div class="form-group col-3 disciplinas" id="disciplina_2" style="<?php echo"$display"; ?>">
-            <label for="id_disciplina_2">Disciplina</label>
             <select class="form-control" name="id_disciplina_2" id="id_disciplina_2" readonly>
 
               <?php 
@@ -206,7 +213,6 @@
             }
           ?>
           <div class="form-group col-3 disciplinas" id="disciplina_3" style="<?php echo"$display"; ?>">
-            <label for="id_disciplina_3">Disciplina</label>
             <select class="form-control" name="id_disciplina_3" id="id_disciplina_3" readonly>
 
               <?php 
@@ -241,7 +247,6 @@
             }
           ?>   
           <div class="form-group col-3 disciplinas" id="disciplina_4" style="<?php echo"$display"; ?>">
-            <label for="id_disciplina_4">Disciplina</label>
             <select class="form-control" name="id_disciplina_4" id="id_disciplina_4" readonly>
               
               <?php 
@@ -266,45 +271,55 @@
                 </option>
             </select>
           </div><!-- /disciplina 4 -->
-          
-          <!-- /disciplinas -->
-        </div>
+        
+        </div><!-- /disciplinas -->
+
+        <hr>
 
         <!-- dificuldade -->
-        <div class="form-group col-6">
-          <label for="id_dificuldade">Dificuldade</label>
-          <select class="form-control" name="id_dificuldade" id="id_dificuldade" readonly>
-
-            <?php 
-              //mostrar a dificuldade atual
-              // registros tabela dificuldade para listar o nome da dificuldade
-              $sql_nome_dificuldade = "SELECT q.id_dificuldade, d.descricao_dif FROM questao q 
-                      JOIN dificuldade d ON q.id_dificuldade = d.id WHERE d.id = '".$dados['id_dificuldade']."'";
-                  
-              $registros_nome_dificuldade = mysqli_query($con, $sql_nome_dificuldade) or 
-                      die("ERRO NA BUSCA DA DISCIPLINA!". mysqli_error($con));
-                  
-              $nome_dificuldade = mysqli_num_rows($registros_nome_dificuldade);
-
-              $dados_nome_dificuldade = mysqli_fetch_array($registros_nome_dificuldade);
-
-              $id_dificuldade_atual = $dados_nome_dificuldade["id_dificuldade"];
-              $dificuldade_atual = $dados_nome_dificuldade["descricao_dif"];
-                  
-            ?>
-            <option value="<?php echo $id_dificuldade_atual; ?>">
-              <?php echo "$dificuldade_atual"; ?>
-            </option>
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-4">
+            <div class="form-group">
+              <label for="id_dificuldade">Dificuldade</label>
+              <select class="form-control" name="id_dificuldade" id="id_dificuldade" readonly>
+                <?php
+                  //mostrar a dificuldade atual
+                  // registros tabela dificuldade para listar o nome da dificuldade
+                  $sql_nome_dificuldade = "SELECT q.id_dificuldade, d.descricao_dif FROM questao q
+                          JOIN dificuldade d ON q.id_dificuldade = d.id WHERE d.id = '".$dados['id_dificuldade']."'";
             
-          </select>
+                  $registros_nome_dificuldade = mysqli_query($con, $sql_nome_dificuldade) or
+                          die("ERRO NA BUSCA DA DISCIPLINA!". mysqli_error($con));
+            
+                  $nome_dificuldade = mysqli_num_rows($registros_nome_dificuldade);
+                  $dados_nome_dificuldade = mysqli_fetch_array($registros_nome_dificuldade);
+                  $id_dificuldade_atual = $dados_nome_dificuldade["id_dificuldade"];
+                  $dificuldade_atual = $dados_nome_dificuldade["descricao_dif"];
+            
+                ?>
+                <option value="<?php echo $id_dificuldade_atual; ?>">
+                  <?php echo "$dificuldade_atual"; ?>
+                </option>
+            
+              </select>
+            </div>
+          </div>
+          <div class="col-4"></div>
         </div><!-- /dificuldade -->
         
         <!-- enunciado -->
-        <div class="form-group col-8">
-          <label for="enunciado">Enunciado</label>
-          <textarea class="form-control" name="enunciado" id="enunciado" cols="30" rows="5" readonly>
-            <?php echo $dados["enunciado"] ?>
-          </textarea>
+        <div class="row">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <div class="form-group">
+              <label for="enunciado">Enunciado</label>
+              <textarea class="form-control" name="enunciado" id="enunciado" cols="30" rows="5" readonly>
+                <?php echo $dados["enunciado"] ?>
+              </textarea>
+            </div>
+          </div>
+          <div class="col-2"></div>
         </div><!-- /enunciado -->
       </section>
 
@@ -315,20 +330,20 @@
                     
                 // case múltiplas escolhas
                 case 'M':   echo "
-                                <div class='row'> 
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                                <div class='row text-center'> 
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                         <h6>Múltiplas Escolhas</h6>
                                     </div>
-                                    <div class='col-1'></div>
-                                </div>";
+                                    <div class='col-2'></div>
+                                </div> <hr>";
             ?>
                 <!-- alternartivas -->
                 <?php
                     // alternativa a
-                    echo "      <div class='row mt-1'>
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                    echo "      <div class='row mb-2'>
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                       <div class='form-check form-check-inline'>
                                         <label class='form-check-label' for='alternativa_correta'>a.</label>
                                         <input class='form-check-input' type='radio' value='A'
@@ -339,21 +354,21 @@
                                             } 
                     echo "                  >"; //fechar o input
                                         
-                    echo "              <textarea class='form-control' cols='110' rows='1' 
+                    echo "              <textarea class='form-control' cols='88' rows='1' 
                                                  maxlength='2000' readonly>
                                             ".$dados["resposta_alt_a"]."
                                         </textarea>
                                       </div>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     "; // fim alternativa a
                 ?>
                 <?php
                     // alternativa b
-                    echo "      <div class='row mt-1'>
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                    echo "      <div class='row mb-2'>
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                       <div class='form-check form-check-inline'>
                                         <label class='form-check-label' for='alternativa_correta'>b.</label>
                                         <input class='form-check-input' type='radio' value='B'
@@ -364,21 +379,21 @@
                                             } 
                     echo "                  >"; //fechar o input
                                         
-                    echo "              <textarea class='form-control' cols='110' rows='1' 
+                    echo "              <textarea class='form-control' cols='88' rows='1' 
                                                  maxlength='2000' readonly>
                                             ".$dados["resposta_alt_b"]."
                                         </textarea>
                                       </div>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     "; // fim alternativa b
                 ?>
                 <?php
                     // alternativa c
-                    echo "      <div class='row mt-1'>
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                    echo "      <div class='row mb-2'>
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                       <div class='form-check form-check-inline'>
                                         <label class='form-check-label' for='alternativa_correta'>c.</label>
                                         <input class='form-check-input' type='radio' value='C'
@@ -389,21 +404,21 @@
                                             } 
                     echo "                  >"; //fechar o input
                                         
-                    echo "              <textarea class='form-control' cols='110' rows='1' 
+                    echo "              <textarea class='form-control' cols='88' rows='1' 
                                                  maxlength='2000' readonly>
                                             ".$dados["resposta_alt_c"]."
                                         </textarea>
                                       </div>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     "; // fim alternativa c
                 ?>
                 <?php
                     // alternativa d
-                    echo "      <div class='row mt-1'>
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                    echo "      <div class='row mb-2'>
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                       <div class='form-check form-check-inline'>
                                         <label class='form-check-label' for='alternativa_correta'>d.</label>
                                         <input class='form-check-input' type='radio' value='D'
@@ -414,21 +429,21 @@
                                             } 
                     echo "                  >"; //fechar o input
                                         
-                    echo "              <textarea class='form-control' cols='110' rows='1' 
+                    echo "              <textarea class='form-control' cols='88' rows='1' 
                                                  maxlength='2000' readonly>
                                             ".$dados["resposta_alt_d"]."
                                         </textarea>
                                       </div>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     "; // fim alternativa d
                 ?>
                 <?php
                     // alternativa e
-                    echo "      <div class='row mt-1'>
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                    echo "      <div class='row mb-2'>
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                       <div class='form-check form-check-inline'>
                                         <label class='form-check-label' for='alternativa_correta'>e.</label>
                                         <input class='form-check-input' type='radio' value='E'
@@ -439,13 +454,13 @@
                                             } 
                     echo "                  >"; //fechar o input
                                         
-                    echo "              <textarea class='form-control' cols='110' rows='1' 
+                    echo "              <textarea class='form-control' cols='88' rows='1' 
                                                  maxlength='2000' readonly>
                                                  ".$dados["resposta_alt_e"]."
                                         </textarea>
                                       </div>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     "; // fim alternativa e
 
@@ -459,24 +474,24 @@
                     case 'D':
                     // titulo dissertativa
                     echo "
-                                <div class='row'> 
-                                    <div class='col-1'></div>
-                                    <div class='col-10'>
+                                <div class='row text-center'> 
+                                    <div class='col-2'></div>
+                                    <div class='col-8'>
                                         <h6>Dissertativa</h6>
                                     </div>
-                                    <div class='col-1'></div>
-                                </div>
+                                    <div class='col-2'></div>
+                                </div><hr>
                     ";
                     // resposta dissertativa
                     echo "
                                 <div class='row'>
-                                    <div class='col-1'></div>
-                                    <div class='form-group col-10'>
-                                        <textarea class='form-control'cols='110' rows='5' maxlength='2000' readonly>
+                                    <div class='col-2'></div>
+                                    <div class='form-group col-8'>
+                                        <textarea class='form-control'cols='30' rows='5' maxlength='2000' readonly>
                                             ".$dados['resposta_dissertativa']."
                                         </textarea>
                                     </div>
-                                    <div class='col-1'></div>
+                                    <div class='col-2'></div>
                                 </div>
                     ";
                     
@@ -488,8 +503,10 @@
                 ?>
                 <!-- /dissertativa -->
 
-        <br><br>
-        <button class="btn btn-outline-dark m-4" type="submit">Excluir Questão</button>
+        
+          <div class="row float-right">
+            <button class="btn btn-outline-dark m-4" type="submit">Excluir Questão</button>
+          </div>
 
       </form>
 
